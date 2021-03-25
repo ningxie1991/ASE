db16ae65a96c3f95f536f8724dbea1b9039c6516
ningxie <ning.xie@uzh.ch>
2021-03-25 11:22:03 +0100

    Update routing

--- finder-service/src/main/java/ifi/ase/ascout/finderservice/controller/HelloController.java
+++ finder-service/src/main/java/ifi/ase/ascout/finderservice/controller/HelloController.java
@@ -1,10 +1,12 @@
 package ifi.ase.ascout.finderservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/finder/")
 public class HelloController {
 
     @GetMapping("/hello")
