6a4849cd74f54d6c313286e358a8240a53e8614e
ningxie <ning.xie@uzh.ch>
2021-03-30 18:27:09 +0200

    Updated client for example

--- ascout-client/src/App.js
+++ ascout-client/src/App.js
@@ -1,36 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import FinderService from './services/FinderService.js';
+import Home from './pages/Home';
 
 export default class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {
-            message: ''
-        };
-    }
-
-    componentDidMount(){
-        FinderService.getMessage().then(response => {
-            this.setState({message: response.data})
-        });
     }
 
     render(){
 
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo"/>
-                    <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                    </p>
-
-                    <h1>{this.state.message}</h1>
-
-                </header>
+               <Home />
             </div>
         );
     }
