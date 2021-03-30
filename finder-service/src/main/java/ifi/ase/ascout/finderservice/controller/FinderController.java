cb74cb673694265b06d1f4e594045f932c3e6d28
ningxie <ning.xie@uzh.ch>
2021-03-30 11:55:47 +0200

    Updated client and backend to use the finder-service

--- finder-service/src/main/java/ifi/ase/ascout/finderservice/controller/FinderController.java
+++ finder-service/src/main/java/ifi/ase/ascout/finderservice/controller/FinderController.java
@@ -1,11 +1,9 @@
 package ifi.ase.ascout.finderservice.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path = "/finder")
 public class FinderController {
 
