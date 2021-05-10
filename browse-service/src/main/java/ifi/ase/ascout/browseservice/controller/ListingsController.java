3aaad4ddd931e7148e1590bd0d5363e5b10e719a
ningxie <ning.xie@uzh.ch>
2021-05-10 15:06:56 +0200

    Update ascout-client deployment files

--- browse-service/src/main/java/ifi/ase/ascout/browseservice/controller/ListingsController.java
+++ browse-service/src/main/java/ifi/ase/ascout/browseservice/controller/ListingsController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping(path = "/browse")
 public class ListingsController {
 
