3c494a4e4181ab9830576baa379d1212a9c4b59a
ningxie <ning.xie@uzh.ch>
2021-05-26 11:20:55 +0200

    Remove vulnerability as reported by sonar cloud

--- calculator-service/src/main/java/ifi/ase/ascout/calculatorservice/servise/CalculatorService.java
+++ calculator-service/src/main/java/ifi/ase/ascout/calculatorservice/servise/CalculatorService.java
@@ -47,10 +47,7 @@ public class CalculatorService implements ICalculatorService{
         //TODO what if name is null : java.lang.NullPointerException
         TravelMode travelMode = TravelMode.valueOf(query.getTravelMode());
         String[] origin_attractions = query.getOrigins();//row names,origin/attraction names
-
-        for(String s :origin_attractions){
-            logger.debug(s);
-        }
+        
         List<NeighborhoodModel> neiList = repository.findAll();//=UTILS.dummyNList();
         if(neiList.size()==0){
             logger.error("Empty response from DB!");
